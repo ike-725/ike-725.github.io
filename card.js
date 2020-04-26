a0083167ec9abd6498580fdc5bfa442d81912e7c
ike-725 <mongoloid617@gmail.com>
2020-04-26 22:22:06 +0900

    カード表示

--- card.js
+++ card.js
@@ -10,16 +10,16 @@ var max = 4 ;
 
 
 var thema=[
-  'https://github.com/ike-725/ike-725.github.io/blob/master/image/0.PNG',
-  'image/1.PNG',
-  'image/2.PNG',
-  'image/3.PNG',
-  'image/4.PNG'
+  './image/0.PNG',
+  './image/1.PNG',
+  './image/2.PNG',
+  './image/3.PNG',
+  './image/4.PNG'
 ];
 
 //document.write(thema[a] + "<br>\n");
 
 
 var a = Math.floor(Math.random() * thema.length);
-document.write(thema[a]);
+document.write('<img src="thema[a]" />');
 //document.write(thema[a]);
